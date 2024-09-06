e7ee7f7bf0a396d880eb360d5fd3778ba72e4177
dee <178827694+dee797@users.noreply.github.com>
2024-09-06 17:26:03 -0600

    Added several new functions
    removeDivs(): Removes all divs of the .row class
    
    addDivs(): this function was originally called
    createDivGrid, but I changed the name for
    consistency. I added a parameter to this function,
    'dimensions', so that it can dynamically
    create and add any number of divs.
    
    changeGrid(): Uses 'prompt' to get an input from
    the user. I implemented input validation so the
    user can't enter a string or a number greater than
    100 or less than 1. Upon successful validation,
    it will call removeDivs and addDivs, and supply
    the user's input as an argument to addDivs.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-function createDivGrid() {
+function addDivs(dimensions) {
     const container = document.querySelector("body > .container");
 
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < dimensions; i++) {
         const divRow = document.createElement("div");
         divRow.classList.add("row");
         container.appendChild(divRow);
 
-        for (let i = 0; i < 16; i++) {
+        for (let i = 0; i < dimensions; i++) {
             const divColumn = document.createElement("div");
             divColumn.classList.add("column");
             divColumn.addEventListener("mouseover", () =>{
@@ -15,10 +15,52 @@ function createDivGrid() {
             divRow.appendChild(divColumn);
         }
     }
-
-    const changeGridBtn = document.createElement("button");
-    changeGridBtn.textContent = "Change grid area"; 
-    document.body.insertBefore(changeGridBtn, container);
 }
 
-createDivGrid();
+
+function removeDivs() {
+    const divRows = document.querySelectorAll("body .row");
+    for (const row of divRows) {
+        row.remove()
+    }
+}
+
+
+function changeGrid() {
+    let dimensions = prompt(`How many squares would you like per side?
+    Note that this will erase your previous sketch pad.`);
+
+    while (dimensions !== null) {
+        const temp = parseInt(dimensions);
+        
+        if (isNaN(temp) || temp > 100 || temp < 1) {
+            alert("Invalid entry. Please enter a number between 1 and 100.");
+            dimensions = prompt(`How many squares would you like per side?\n
+Note that this will erase your previous sketch pad.`);
+            
+        } else {
+            dimensions = temp;
+            break
+        }
+    }
+
+    if (dimensions === null) throw "User clicked cancel";
+
+    removeDivs();
+    addDivs(dimensions);
+}
+
+
+function createButton() {
+    const changeGridBtn = document.createElement("button");
+    const container = document.querySelector("body .container");
+
+    changeGridBtn.textContent = "Change grid area"; 
+    document.body.insertBefore(changeGridBtn, container);
+
+    changeGridBtn.addEventListener("click", () => changeGrid())
+}
+
+
+addDivs(16);
+createButton();
