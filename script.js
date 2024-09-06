83ed1a147d4e698c52d78107a17b02a0f3208280
dee <178827694+dee797@users.noreply.github.com>
2024-09-06 13:24:33 -0600

    Added nested for loop
    Added a nested for loop in the function
    createDivGrid() as I realized I was previously only
    appending one column div per row. With this new
    loop, there are 16 column divs per row.

--- script.js
+++ script.js
@@ -4,10 +4,16 @@ function createDivGrid() {
     for (let i = 0; i < 16; i++) {
         const divRow = document.createElement("div");
         divRow.classList.add("row");
+        divRow.innerText = `\u00A0`;
         container.appendChild(divRow);
 
-        const divColumn = document.createElement("div");
-        divColumn.classList.add("column");
-        divRow.appendChild(divColumn);
+        for (let i = 0; i < 16; i++) {
+            const divColumn = document.createElement("div");
+            divColumn.classList.add("column");
+            divColumn.innerText = `\u00A0`;
+            divRow.appendChild(divColumn);
+        }
     }
 }
+
+createDivGrid();
